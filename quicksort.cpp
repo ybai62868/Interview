6ab1e91fbc6f67c9147bf672012879ebec0d8ac9
Yang Bai <ybai62868@gmail.com>
2016-07-04 11:17:12 +0800

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,20 +1,37 @@
-void quicksort( vector<int>&v,int left,int right ){
+//O(nlogn)
+# include <cstdio>
+# include <iostream>
+using namespace std;
+# define MAX 123456
+int a[MAX];
+int n;
+void quicksort( int * v,int left,int right )
+{
     if ( left < right ){
         int key = v[left];
-        int lo = left;
-        int hi = right;
-        while ( lo < hi ){
-            while ( lo < hi && v[hi] > key ){
-                hi--;
-            }
-            v[lo] = v[hi];
-            while ( lo < hi && v[lo] < key ){
-                lo++;
-            }
-            v[hi] = v[lo];
+        int low = left;
+        int hight = right;
+        while ( low < hight ){
+           while ( low < hight&&v[hight] > key ){
+                hight--;
+           }
+           v[low] = v[hight];
+           while ( low < hight&&v[low] < key ){
+                low++;
+           }
+           v[hight] = v[low];
         }
-        v[lo] = key;
-        quicksort(v,left,lo-1);
-        quicksort(v,lo+1,right);
+        v[low] = key;
+        quicksort(v,left,low-1);
+        quicksort(v,low+1,right);
     }
+}
+int main(void)
+{
+    cin>>n;
+    for ( int i = 0;i < n;i++ ) cin>>a[i];
+    quicksort(a,0,n-1);
+    for ( int i = 0;i < n;i++ ) cout<<a[i]<<" ";
+    cout<<endl;
+    return 0;
 }
